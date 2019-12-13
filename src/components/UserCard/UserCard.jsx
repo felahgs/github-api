c2189fae22d7c83c7d8e5c7339db0ca5bbb1bd7f
FelahGS <fgsouza93@hotmail.com>
2019-12-13 11:58:06 -0300

    Add custom Loader animation

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -3,12 +3,16 @@ import './UserCard.css';
 
 const UserCard = (props) => {
 
+    const onClick = () => {
+        console.log('click', props.user)
+    }
+
     return (
-        <div className="user-card">
+        <div className="user-card" onClick={() => {onClick()}}>
             <div className="user-avatar">
                 <img src={props.user.avatar_url} alt="user avatar"/>
             </div>
-            <div className="user-name">
+            <div  className="user-name">
                 <b>{props.user.login}</b>
             </div>
         </div>
