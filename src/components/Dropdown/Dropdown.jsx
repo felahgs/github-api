a3c49c6efd3647913dfa55ab82523985279be8f6
Felipe <fgsouza93@hotmail.com>
2020-01-20 23:01:31 -0300

    adjust dropdown size

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,18 +1,28 @@
-import React, {useEffect} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import Users from '../../containers/UserCardContainer';
 import './Dropdown.css';
 
 const Dropdown = (props) => {
+  const dropdownRef = useRef();
+  const [isVisible, setIsVisible] = useState(true)
 
-useEffect(() => {console.log("Dropdown rendered")}, [])
-useEffect(() => {console.log("Dropdown: input props changed", props.input)}, [props.input])
+  useEffect(() => {console.log("Dropdown rendered")}, [])
+  useEffect(() => {console.log("Dropdown: input props changed", props.input)}, [props.input])
+  useEffect(() => { document.addEventListener("click", handleClickOutside, false)}, []);
 
-return (
-  <div className="dropdown-component">
-    {props.input.length === 0 ? null : <Users input={props.input}/>}
-  </div>
-)
-  
-}
+  const handleClickOutside = event => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      console.log('clicked outside');
+      setIsVisible(false);
+    }
+  };
+
+  return (
+    <div className="dropdown-component" ref={dropdownRef}>
+      {props.input.length === 0 ? null : <Users input={props.input}/>}
+    </div>
+  )
+    
+  }
 
 export default Dropdown;
